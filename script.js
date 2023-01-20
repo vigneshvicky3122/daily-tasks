80acb005f5f2cf75546f7008e19141a7980e5b8b
vigneshvicky3122@gmail.com <vigenshvicky3122@gamil.com>
2023-01-20 10:41:58 +0530

    final

--- script.js
+++ script.js
@@ -5,27 +5,35 @@ console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
 
 // 2.) display the all countries flags in console
 
-let flags = fetch("https://restcountries.com/v3.1/all")
-  .then((res) => res.json())
-  .then((data) => {
-    for (let index = 0; index < data.length; index++) {
-      const element = data[index].flags;
-      console.log(element);
-    }
-  })
-  .catch((err) => console.log(err));
+let flags = new XMLHttpRequest();
+flags.open("GET", "https://restcountries.com/v3.1/all");
+flags.onload = function () {
+  let result = JSON.parse(flags.response);
+  for (let index = 0; index < result.length; index++) {
+    const element = result[index];
+    console.log(element.flags);
+  }
+};
+flags.onerror = function () {
+  console.error();
+};
+flags.send();
 
 // 3.)print the all countries name,region,subregion,population
 
-let data = fetch("https://restcountries.com/v3.1/all")
-  .then((res) => res.json())
-  .then((data) => {
-    for (let index = 0; index < data.length; index++) {
-      const element = data[index];
-      console.log("name :" + element.name.common);
-      console.log("region :" + element.region);
-      console.log("subregion :" + element.subregion);
-      console.log("population :" + element.population);
-    }
-  })
-  .catch((err) => console.log(err));
+let data = new XMLHttpRequest();
+data.open("GET", "https://restcountries.com/v3.1/all");
+data.onload = function () {
+  let result = JSON.parse(data.response);
+  for (let index = 0; index < result.length; index++) {
+    const element = result[index];
+    console.log("name :" + element.name.common);
+    console.log("region :" + element.region);
+    console.log("subregion :" + element.subregion);
+    console.log("population :" + element.population);
+  }
+};
+data.onerror = function () {
+  console.error();
+};
+data.send();
